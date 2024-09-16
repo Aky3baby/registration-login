3da0fe3ec6d24b06d85e2be8ddd714306f109c6b
linda <lindabrokye1993@gmail.com>
2024-09-16 11:46:03 +0000

    first commit

---
+++ scripts/register.js
@@ -0,0 +1,11 @@
+// select form by id
+const registerForm =document.getElementById('register-form');
+
+// Handle form submit event
+registerForm.onsubmit = function() {
+    // collect username
+    // collect email
+    // collect password
+    // save user information
+    // send confirmation email 
+} 
